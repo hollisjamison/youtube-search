45bea7d222c886016ef1d6672b84635b24b58de2
Hollis Jamison <Hollisjamison@gmail.com>
2021-09-27 23:13:05 -0400

    Intial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Youtube Search
+Simple Youtube app to search videos and watch them all in React!
---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import Youtube from "../../apis/youtube";
+import VideoList from "../../components/VideoList/VideoList";
+import VideoDetail from "../../components/VideoDetail/VideoDetail";
+import "./MainPage.css";
+
+const MainPage = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  useEffect(() => {
+    onTermSubmit("React Javascript");
+  }, []);
+
+  const onTermSubmit = async (term) => {
+    const response = await Youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
+  };
+
+  return (
+    <div className="ui container">
+      <SearchBar onFormSubmit={onTermSubmit} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MainPage
